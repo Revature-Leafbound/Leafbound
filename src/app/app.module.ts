8b6c1581f79b304aafa4071be14b7621f4e17709
sangar3 <55110746+sangar3@users.noreply.github.com>
2022-06-14 23:21:45 -0500

    added base checkout page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialExampleModule} from '../material.module';
+import { checkout } from './checkout';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [checkout],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [checkout],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/checkout.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic Inputs
+ */
+@Component({
+  selector: 'checkout',
+  styleUrls: ['checkout.css'],
+  templateUrl: 'checkout.html',
+})
+export class checkout {}
+
