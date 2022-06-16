db685108fbb6be6f9edd8c762eb0dacb4550a4ac
CuChulainn64 <patrick.miller1@snhu.edu>
2022-06-16 15:17:39 -0400

    created template component for user profile

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,6 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
+  hide = true;
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
   constructor() { }
 
   ngOnInit(): void {
