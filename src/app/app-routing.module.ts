3bfe25c3fde06bea8a08e5faab5726b89bc521e9
briiitelord <96019784+briiitelord@users.noreply.github.com>
2022-06-17 15:14:10 -0400

    Updated testing files, added app-routing module test file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
+
+//**tslib can't be found */
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
