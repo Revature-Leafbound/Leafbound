4cf85f594b6ce398b99c44570047deb6db97d82f
CuChulainn64 <patrick.miller1@snhu.edu>
2022-06-20 16:54:15 -0400

    fixed the get request for getting user order history

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -16,9 +16,10 @@ export class OrderService {
     const headers = {
       'Authorization': 'Bearer ' + localStorage.getItem('token')
     }
-    
+    //get the customer's id number from local storage
+    const customerId = localStorage.getItem('customerId');
 
-    return this.http.get<Order[]>('http://localhost:8080/api/order/history', { headers });
+    return this.http.get<Order[]>(`http://localhost:8080/api/order/customer/{customerId}`, { headers });
    // return this.http.get<Order[]>('/api/order/all');
    
    }
