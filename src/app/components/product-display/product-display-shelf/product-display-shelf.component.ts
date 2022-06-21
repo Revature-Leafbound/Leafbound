45ba654c9f126b995b8363424166eeac63382243
FullSol <ceraines@student.fullsail.edu>
2022-06-21 13:32:37 -0400

    added router and used redirect

--- src/app/components/product-display/product-display-shelf/product-display-shelf.component.ts
+++ src/app/components/product-display/product-display-shelf/product-display-shelf.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { PRODUCTS } from 'src/app/models/mock-products';
 import { Product } from 'src/app/models/product';
 import { User } from 'src/app/models/user';
@@ -16,13 +17,18 @@ export class ProductDisplayShelfComponent implements OnInit {
   products: any;
   selectedProduct?: Product;
   testingProducts = PRODUCTS;
-  user?:User;
-  cartId?:number;
-
+  user?: User;
+  cartId?: number;
 
   url = environment.apiUrl + '/product/all';
 
   @Output() msgToSibling = new EventEmitter<Product>();
+
+  constructor(
+    private appService: ProductService,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   getProducts(): void {
     this.http.get(this.url).subscribe((data) => (this.products = data));
@@ -33,33 +39,38 @@ export class ProductDisplayShelfComponent implements OnInit {
   }
 
   onAddToCart(product: Product): void {
-
     //addToCart(product);
     console.log(product);
-    
+
     // get user to insert into body
-    const body = { 
+    const body = {
       product: product,
       quantity: 1,
-      user: this.user
-    }
+      user: this.user,
+    };
 
-    // Grab token to be inserted
-    const headers = {
-      'Authorization':'Bearer the-token'
-    }
+    const token = localStorage.getItem('token');
 
-    // make post request
-    this.http.put<any>('http://localhost:8080/api/v1/cart', body, { headers }).subscribe(data => {
-      this.cartId = data.id
-    })
+    if (token === null) {
+      this.router.navigate(['/login']);
+    } else {
+      // Grab token to be inserted
+      const headers = {
+        Authorization: token,
+      };
+
+      // make post request
+      this.http
+        .put<any>('http://localhost:8080/api/v1/cart', body, { headers })
+        .subscribe((data) => {
+          this.cartId = data.id;
+        });
+    }
   }
 
   getId(isbn: string): string {
     return 'https://covers.openlibrary.org/b/isbn/' + isbn + '-M.jpg';
   }
-
-  constructor(private appService: ProductService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.appService.currentSelectedProduct.subscribe(
--- src/app/components/product-display/product-item/product-item.component.ts
+++ src/app/components/product-display/product-item/product-item.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { User } from 'src/app/models/user';
 import { ProductService } from 'src/app/services/product.service';
@@ -12,35 +13,50 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductItemComponent implements OnInit {
   @Input()
   currentSelectedProduct?: Product;
-  user?:User;
-  cartId?:any;
+  user?: User;
+  cartId?: any;
+
+  constructor(
+    private appService: ProductService,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   onAddToCart(): void {
     console.log(this.currentSelectedProduct);
 
     // get user to insert into body
-    const body = { 
+    const body = {
       product: this.currentSelectedProduct,
       quantity: 1,
-      user: this.user
+      user: this.user,
+    };
+
+    const token = localStorage.getItem('token');
+
+    if (token === null) {
+      this.router.navigate(['/login']);
+    } else {
+      // Grab token to be inserted
+      const headers = {
+        Authorization: token,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      };
+
+      // make post request
+      this.http
+        .put<any>('http://localhost:8080/api/v1/cart', body, { headers })
+        .subscribe((data) => {
+          this.cartId = data.id;
+        });
     }
-
-    // Grab token to be inserted
-    const headers = {
-      'Authorization':'Bearer the-token'
-    }    
-
-    // make post request
-    this.cartId = this.http.put<any>('http://localhost:8080/api/v1/cart', body, { headers }).subscribe(data => {
-      this.cartId = data.id
-    })
   }
 
   getId(isbn: string): string {
     return 'https://covers.openlibrary.org/b/isbn/' + isbn + '-L.jpg';
   }
-
-  constructor(private appService: ProductService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.appService.currentSelectedProduct.subscribe(
