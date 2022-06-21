2fedfc430af426d105ead0463ea303fe9774bf1c
FullSol <ceraines@student.fullsail.edu>
2022-06-20 22:00:45 -0400

    import fix

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,26 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { PRODUCTS } from '../models/mock-products';
 import { Product } from '../models/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
-  product?:Product;
+  product?: Product;
   private selectedProduct = new BehaviorSubject(this.product);
   currentSelectedProduct = this.selectedProduct.asObservable();
- 
-  constructor(private http:HttpClient) { }
 
-  updateSelectedProduct(product:Product) {
+  constructor(private http: HttpClient) {}
+
+  updateSelectedProduct(product: Product) {
     this.selectedProduct.next(product);
   }
 
-  getProducts(): Observable<Product[]>{
+  getProducts(): Observable<Product[]> {
     const products = of(PRODUCTS);
     return products;
   }
-
 }
