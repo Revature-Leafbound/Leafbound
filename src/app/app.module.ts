eae6a47abdaba261bf7a2d725366d64b77380922
FullSol <ceraines@student.fullsail.edu>
2022-06-21 11:08:52 -0400

    fixed whatever error that was.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { ProductDisplayComponent } from './components/product-display/product-di
 import { ProductDisplayShelfComponent } from './components/product-display/product-display-shelf/product-display-shelf.component';
 import { ProductItemComponent } from './components/product-display/product-item/product-item.component';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
-
+import { RegistrationForm } from './components/registration-form/registration-form.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegistrationForm } from './components/registration-form/registration-form.component';
+
 import { MaterialExampleModule } from '../material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
