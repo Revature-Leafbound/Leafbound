68cb0bd76ce75b35b77ec4c209158107eee610b2
FullSol <ceraines@student.fullsail.edu>
2022-06-21 12:24:07 -0400

    added register submit

--- src/app/components/registration-form/registration-form.component.ts
+++ src/app/components/registration-form/registration-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: ' RegistrationForm',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration-form.component.css'],
 })
 export class RegistrationFormComponent implements OnInit {
-  constructor() {}
+  registerForm = this.formBuilder.group({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {}
+  onSubmit(): void {}
 }
